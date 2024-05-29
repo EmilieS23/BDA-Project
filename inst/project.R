77b8c1f9cf4b883bd7c9c43e9127c6b1afcd320c
EmilieS23 <91272231+EmilieS23@users.noreply.github.com>
2024-05-29 15:47:20 +0200

    Add files via upload

---
+++ inst/project.R
@@ -0,0 +1,27 @@
+### Main script of the project
+
+###My species: Vulpes vulpes (sp1) & Microtus arvalis (sp2)
+###Analyse relation prey-predator
+
+###Maps
+
+source('src/import_data.R')
+
+source('src/ecosystems.R')
+
+source('src/elevation.R')
+
+source('src/world_clim_data.R')
+
+source('src/3d_map_points.R')
+
+source('src/NDVI.R')
+
+###GGPLOT   
+
+source('src/ggplot.R')
+
+###Statistiques
+
+source('src/kernel_density_map.R') #pour l'instant ne fonctionne pas
+
