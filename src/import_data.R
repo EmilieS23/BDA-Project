300ee8f2c66aaa032241fbaff13ec9662e3752df
EmilieS23 <91272231+EmilieS23@users.noreply.github.com>
2024-05-30 11:04:02 +0200

    Update import_data.R

--- src/import_data.R
+++ src/import_data.R
@@ -1,17 +1,7 @@
-#faire les démarches pour avoir une première matrix_full donc avec les données de gbif et inat + loger les pacakges
-
-# Load required libraries
-
-library(rgbif)         # For accessing GBIF occurrence data
-library(rnaturalearth) # For obtaining spatial data
-library(ggplot2)       # For data visualization
-library(rinat)         # For accessing iNaturalist occurrence data
-library(raster)        # For spatial operations
-
 #Species and data with gbif --> 2 species
 
-sp1 <- c("Vulpes vulpes")
-sp2 <- c("Microtus arvalis")
+sp1 <- c("Vulpes vulpes") #renard roux
+sp2 <- c("Microtus arvalis") #campagnol commun
 
 gbif_data_sp1 <- occ_data(scientificName = sp1, hasCoordinate = TRUE, limit = 5000)
 gbif_data_sp2 <- occ_data(scientificName = sp2, hasCoordinate = TRUE, limit = 5000)
@@ -23,26 +13,12 @@ gbif_data_sp2CH <- occur[occur$country == "Switzerland",]
 
 Switzerland <- ne_countries(scale = "medium", returnclass = "sf", country = "Switzerland")
 
-#ggplot(data = Switzerland) +
- # geom_sf() +
-  #geom_point(data = gbif_data_sp1CH, aes(x = decimalLongitude, y = decimalLatitude), size = 4, 
-   #          shape = 23, fill = "orange") + theme_classic()
-
-
-#ggplot(data = Switzerland) +
- # geom_sf() +
-  #geom_point(data = gbif_data_sp2CH, aes(x = decimalLongitude, y = decimalLatitude), size = 4, 
-   #          shape = 23, fill = "grey") + theme_classic()
-
-
 # Extract relevant data from GBIF occurrences for sp1
 
 sp <- gbif_data_sp1CH$species
 latitudesp <- gbif_data_sp1CH$decimalLatitude
 longitudesp <- gbif_data_sp1CH$decimalLongitude
 sourcesp <- rep("gbif", length(sp1))
-
-#data frame for sp1
 
 data_gbif_sp1 <- data.frame(sp, latitudesp, longitudesp, sourcesp)
 
@@ -53,38 +29,20 @@ latitudesp <- gbif_data_sp2CH$decimalLatitude
 longitudesp <- gbif_data_sp2CH$decimalLongitude
 sourcesp <- rep("gbif", length(sp2))
 
-#data frame for sp2
-
 data_gbif_sp2 <- data.frame(sp, latitudesp, longitudesp, sourcesp)
 
-#####
-#####
-#####
+#donc ici j'ai mes données gbif latitudes/longitudes des mes 2 espèces en Suisse 
 
 #Species and data with inat 
 
-# Retrieve iNaturalist occurrence data for the same species in Switzerland
 sp1_inat <- get_inat_obs(query = "Vulpes vulpes", place_id = "switzerland")
 sp2_inat <- get_inat_obs(query = "Microtus arvalis", place_id = "switzerland")
 
-# Plot iNaturalist occurrences on a map of Switzerland
-#ggplot(data = Switzerland) +
- # geom_sf() +
-  #geom_point(data = sp1_inat, aes(x = longitude, y = latitude), size = 4, 
-   #          shape = 23, fill = "orange") + theme_classic()
-
-#ggplot(data = Switzerland) +
- # geom_sf() +
-  #geom_point(data = sp2_inat, aes(x = longitude, y = latitude), size = 4, 
-   #          shape = 23, fill = "grey") + theme_classic()
-
-##################################################
 sp <- sp1_inat$scientific_name
 latitudesp <- sp1_inat$latitude
 longitudesp <- sp1_inat$longitude
 sourcesp <- rep("inat", length(sp1))
 
-# Create a data frame for iNaturalist data
 datasp1_inat <- data.frame(sp, latitudesp, longitudesp, sourcesp)
 
 sp <- sp2_inat$scientific_name
@@ -92,13 +50,9 @@ latitudesp <- sp2_inat$latitude
 longitudesp <- sp2_inat$longitude
 sourcesp <- rep("inat", length(sp2))
 
-# Create a data frame for iNaturalist data
 datasp2_inat <- data.frame(sp, latitudesp, longitudesp, sourcesp)
 
-###############################################################################
-###############################################################################
-###############################################################################
-############ Bind iNaturalist and GBIF data
+#Comme avant, latitude et longitude de inat pour mes 2 espèces
 
 # Combine GBIF and iNaturalist data frames
 matrix_full <- rbind(datasp1_inat, data_gbif_sp1,datasp2_inat, data_gbif_sp2)
